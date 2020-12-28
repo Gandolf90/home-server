37d7c54344f637ddd3fbc5b902cdd1c1406c1257
josua <josua_koenig@icloud.com>
2020-12-28 21:13:30 +0100

    added changes

---
+++ nextcloud/config/config.php
@@ -0,0 +1,25 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'oc385c5opykl',
+  'passwordsalt' => 'BXMpT8EW2YkNfD6P/HKSFdfx38xxFo',
+  'secret' => 'iXjNfhw7O58fR/UvyL1+FUz9CZ6sJz55t8kdySxu0vO2GtSW',
+  'trusted_domains' => 
+  array (
+    0 => 'koenig-server.icgarsas8yis6ivm.myfritz.net',
+  ),
+  'datadirectory' => '/var/www/html/data',
+  'dbtype' => 'mysql',
+  'version' => '20.0.4.0',
+  'overwrite.cli.url' => 'https://koenig-server.icgarsas8yis6ivm.myfritz.net',
+  'dbname' => 'nextcloud',
+  'dbhost' => 'db',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => 'nextcloud',
+  'dbpassword' => '21340asslfjkl1230daslk',
+  'installed' => true,
+  'theme' => '',
+  'loglevel' => 2,
+  'maintenance' => false,
+);
